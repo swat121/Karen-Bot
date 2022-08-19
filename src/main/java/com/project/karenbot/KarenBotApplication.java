5995f02accc6c265c954807ffe6f0341eec79106
Laptop <stark2542@gmail.com>
2022-08-19 15:32:08 +0300

    Test: running this code on raspberry

--- src/main/java/com/project/karenbot/KarenBotApplication.java
+++ src/main/java/com/project/karenbot/KarenBotApplication.java
@@ -1,10 +1,13 @@
 package com.project.karenbot;
 
+import com.project.karenbot.config.UrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UrlConfig.class)
 public class KarenBotApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/karenbot/service/BotService.java
+++ src/main/java/com/project/karenbot/service/BotService.java
@@ -1,5 +1,6 @@
 package com.project.karenbot.service;
 
+import com.project.karenbot.config.UrlConfig;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,15 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @AllArgsConstructor
 public class BotService extends TelegramLongPollingBot {
-    private final String chatID = "250412288";
-    private final String url = "http://localhost:8080//";
+    private final UrlConfig urlConfig;
+    private ArrayList<String> chatID;
+    private boolean status;
+    private final String url = "http://192.168.0.102:8080/";
     private final RestTemplate restTemplate;
     @Override
     public String getBotUsername() {
@@ -35,19 +39,39 @@ public class BotService extends TelegramLongPollingBot {
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
+        chatID.add("250412288");
+        chatID.add("600017498");
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
+            String chat = message.getChatId().toString();
                 switch (message.getText()) {
                     case "/start":
                         sendMsgForButton(message, new String[]{"Gazebo"}, new String[]{});
                         break;
                     case "Gazebo":
-                        ResponseEntity<String> response
-                                = restTemplate.getForEntity(url + "garry/setting/relay1", String.class);
+                        for (int i = 0; i < chatID.size(); i++){
+                            if (Objects.equals(chatID.get(i), chat)){
+                                status = true;
+                                break;
+                            }else {
+                                status = false;
+                            }
+                        }
+                        if (status) {
+                            ResponseEntity<String> response
+                                    = restTemplate.getForEntity(url + "patric/setting/relay1", String.class);
+                            execute(SendMessage
+                                    .builder()
+                                    .chatId(message.getChatId().toString())
+                                    .text(response.getBody())
+                                    .build());
+                        }
+                        break;
+                    case "ID":
                         execute(SendMessage
                                 .builder()
-                                .chatId(chatID)
-                                .text(response.getBody())
+                                .chatId(message.getChatId().toString())
+                                .text(message.getChatId().toString())
                                 .build());
                         break;
             }
