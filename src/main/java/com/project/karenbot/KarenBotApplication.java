56170c3e4377f5c7e2d911c1cf16479997bd4c02
swat121 <stark2542@gmail.com>
2022-09-25 18:40:43 +0300

    Fix bug: delete unuse class

--- src/main/java/com/project/karenbot/KarenBotApplication.java
+++ src/main/java/com/project/karenbot/KarenBotApplication.java
@@ -2,13 +2,12 @@ package com.project.karenbot;
 
 import com.project.karenbot.config.BotConfig;
 import com.project.karenbot.config.DataConfig;
-import com.project.karenbot.config.UrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({UrlConfig.class, DataConfig.class, BotConfig.class})
+@EnableConfigurationProperties({DataConfig.class, BotConfig.class})
 public class KarenBotApplication {
 
     public static void main(String[] args) {
